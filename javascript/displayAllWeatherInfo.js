03ffb0f9badbd43b23d91e9d5931b669a25626ac
miwa <61976102+miwa223@users.noreply.github.com>
2023-08-11 16:25:01 -0700

    fix to return lat and lng

--- javascript/displayAllWeatherInfo.js
+++ javascript/displayAllWeatherInfo.js
@@ -1,13 +1,9 @@
-import { displayCityName } from "./cityName";
 import { displayCurrentWeather } from "./currentWeather";
 import { displayDailyForecast } from "./dailyForecast";
 import { displayThreeHourRange } from "./threeHourRange";
 
-export const CITY_NAME = "Vancouver";
-
-export const displayAllWeatherInfo = (city = CITY_NAME) => {
-  displayCityName(city);
-  displayCurrentWeather(city);
-  displayDailyForecast(city);
-  displayThreeHourRange(city);
+export const displayAllWeatherInfo = (lat, lng) => {
+  displayCurrentWeather(lat, lng);
+  displayDailyForecast(lat, lng);
+  displayThreeHourRange(lat, lng);
 };
--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,12 @@
 import { displayCurrentWeather } from "./currentWeather";
 import { displayDailyForecast } from "./dailyForecast";
 import { displayThreeHourRange } from "./threeHourRange";
-import { getUserLocation } from "./userLocation";
 import { searchCityName } from "./searchInput";
 
 function main() {
   displayCurrentWeather();
   displayDailyForecast();
   displayThreeHourRange();
-  getUserLocation();
   searchCityName();
 }
 

1313b8eafa56dffb437c4a5290a6cf840215ccf3
Kana Taguchi <mukrabwy103@gmail.com>
2023-08-10 21:16:14 -0700

    Add one more comment

--- javascript/displayAllWeatherInfo.js
+++ javascript/displayAllWeatherInfo.js
@@ -7,5 +7,6 @@ export const CITY_NAME = 'Vancouver';
 export const displayAllWeatherInfo = (city = CITY_NAME) => {
   displayCityName(city);
   displayCurrentWeather(city);
+  // TODO FIx params to lad and long
   getDailyThreeHoursForecast(city);
 };

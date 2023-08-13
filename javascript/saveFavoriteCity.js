9fbf9f0aea8b7906381c711914260707fd4a605a
rindy1123 <genryusinbaou@gmail.com>
2023-08-12 22:49:58 -0700

    fix: indexOf to findIndex

--- javascript/saveFavoriteCity.js
+++ javascript/saveFavoriteCity.js
@@ -7,7 +7,9 @@ export const saveFavoriteCity = () => {
     let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
     const selectedCityData = onPlaceChanged();
-    const index = favorites.indexOf(selectedCityData.name);
+    const index = favorites.findIndex(
+      (city) => city.name === selectedCityData.name
+    );
 
     if (index === -1) {
       favorites.push(selectedCityData);

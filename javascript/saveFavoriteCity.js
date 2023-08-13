26bc2df3cf75ac0545939a56ad84e7279b5e6d4b
Atsuki Kitada <canvanvam@gmail.com>
2023-08-12 17:51:31 -0700

    fix the error issue 1

--- javascript/saveFavoriteCity.js
+++ javascript/saveFavoriteCity.js
@@ -8,20 +8,22 @@ export const saveFavoriteCity = () => {
     let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
     const selectedCityData = onPlaceChanged();
-    const index = favorites.findIndex(
-      (city) => city.name === selectedCityData.name
-    );
+    if (selectedCityData) {
+      const index = favorites.findIndex(
+        (city) => city.name === selectedCityData.name
+      );
 
-    if (index === -1) {
-      favorites.push(selectedCityData);
-      localStorage.setItem("favorites", JSON.stringify(favorites));
-      fav.style.fill = "#e9a14f";
-    } else {
-      favorites.splice(index, 1);
-      localStorage.setItem("favorites", JSON.stringify(favorites));
-      fav.style.fill = "#fdfdfd";
+      if (index === -1) {
+        favorites.push(selectedCityData);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+        fav.style.fill = "#e9a14f";
+      } else {
+        favorites.splice(index, 1);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+        fav.style.fill = "#fdfdfd";
+      }
+
+      displayFavoriteCities();
     }
-
-    displayFavoriteCities();
   });
 };
